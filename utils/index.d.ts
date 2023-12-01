3ec9d5c062efeb1b6bb42cf5a8efe9c5a8356be9
darksunlight <darksunlight210@gmail.com>
2023-11-30 20:55:50 -0800

    Initial commit

---
+++ utils/index.d.ts
@@ -0,0 +1,20 @@
+interface Array {
+    sum(): Array;
+    max(): Array;
+    min(): Array;
+    toInt(): Array<number>;
+    sortAsc(): Array;
+    sortDesc(): Array;
+    windowed(size: number, step = 1, partialWindows = false): Array;
+    chunks(): Array;
+    permute(): Array;
+}
+
+interface String {
+    toInt(): number;
+    lines(): string[];
+}
+
+interface Number {
+    toChar(): string;
+}
---
+++ utils/index.js
@@ -0,0 +1,72 @@
+Array.prototype.sum = function(val = 0) {
+    return this.reduce((a, c) => a + c, val);
+}
+Array.prototype.max = function() {
+    return Math.max(...this);
+};
+Array.prototype.min = function() {
+    return Math.min(...this);
+};
+Array.prototype.toInt = function() {
+    return this.map(x => +x);
+}
+Array.prototype.sortAsc = function() {
+    return this.sort((a, b) => a - b);
+}
+Array.prototype.sortDesc = function() {
+    return this.sort((a, b) => b - a);
+}
+Array.prototype.windowed = function(size, step = 1, partialWindows = false) {
+    return Array.from(
+        { length: Math.floor(this.length / step) - Math.ceil(size / step) + 1 + partialWindows },
+        (_, i) => this.slice(i * step, i * step + size)
+    )
+}
+Array.prototype.chunks = function(x) { // like last year I got this from https://stackoverflow.com/a/37826698
+    return this.reduce((p, c, i) => {
+        const ci = Math.floor(i / x);
+        if (!p[ci]) p[ci] = [];
+        p[ci].push(c);
+        return p;
+    }, []);
+}
+Array.prototype.permute = function() {
+    var length = this.length,
+        result = [this.slice()],
+        c = new Array(length).fill(0),
+        i = 1, k, p;
+    while (i < length) {
+        if (c[i] < i) {
+            k = i % 2 && c[i];
+            p = this[i];
+            this[i] = this[k];
+            this[k] = p;
+            ++c[i];
+            i = 1;
+            result.push(this.slice());
+        } else {
+            c[i] = 0;
+            ++i;
+        }
+    }
+    return result;
+}
+
+global.readInput = function() {
+    return require("fs").readFileSync('./input.txt', 'utf-8');
+}
+global.readTInput = function() {
+    return require("fs").readFileSync('./test.txt', 'utf-8');
+}
+global.print = function(...args) {
+    console.log(...args);
+}
+String.prototype.toInt = function() {
+    return this.charCodeAt(0);
+}
+String.prototype.lines = function() {
+    return this.split('\n');
+}
+Number.prototype.toChar = function() {
+    return String.fromCharCode(this);
+}
