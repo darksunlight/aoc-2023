516742cd1e469b809f7b35005215a9dd76fd0369
darksunlight <darksunlight210@gmail.com>
2023-12-07 21:25:01 -0800

    day 2 golf 2

---
+++ day02/golf2.js
@@ -0,0 +1,1 @@
+require("../utils");print(readInput().lines().map(l=>["red","green","blue"].map(c=>l.split`: `[1].split`; `.map(l=>l.split`, `.map(x=>[+x.split` `[0],x.split` `[1]])).flat().filter(u=>u[1]==c).max(0)).product()).sum())
